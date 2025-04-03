bb9e1959d49e05576a2ff6637f2c2035cd5ac322
MarwaMashkour <marwa.mashkour@elev.ga.ntig.se>
2025-04-03 18:28:20 +0200

    ändrat lite överallt:)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-//JAVASCRIPT FIL HÄR
+const hamburgare = document.querySelector(".hamburgare");
+const nav = document.querySelector(".navlankar");
+
+hamburgare.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    alert("BU");
+    nav.classList.toggle("active");
+}
