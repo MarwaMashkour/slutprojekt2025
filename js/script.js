fcc43c4a0b45bddc711c5844af397040986a40cf
MarwaMashkour <marwa.mashkour@elev.ga.ntig.se>
2025-04-25 14:38:29 +0200

    komplettering

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".navlankar");
 
 hamburgare.addEventListener('click', toggleMenu);
 function toggleMenu() {
-    alert("BU");
+   
     nav.classList.toggle("active");
+   
 }
